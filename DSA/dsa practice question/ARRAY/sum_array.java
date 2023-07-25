47494e21c024dc694f82ed972752f8c555a9807c
ROHAN GOYAL <130501913+rohangoyal3012@users.noreply.github.com>
2023-07-25 23:32:03 +0530

    Create sum_array.java

---
+++ DSA/dsa practice question/ARRAY/sum_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sum_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("enter the array");
+        int arr[] = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum = sum+arr[i];
+        }
+        System.out.println("sum of the array is "+sum);
+
+
+
+
+
+    }
+}
